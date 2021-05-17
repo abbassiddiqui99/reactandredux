babe870f5f6d456239f41bc11e4b7d233fc2f6dd
Abbas Hussain <abbas.hussain@intelligenes.net>
2021-05-17 16:33:49 +0500

    added SHALLOW case enzyme

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,13 +1,9 @@
+import { shallow } from "enzyme";
 import React from "react";
-import reactDom from "react-dom";
-import ReactDOM from "react-dom";
 import App from "../App";
+import CommentBox from "../CommentBox";
 
 it("shows a comment box", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  // Look inside the div
-  // And see whether the commentbox exists.
-  expect(div.innerHTML).toContain("CommentBox");
-  reactDom.unmountComponentAtNode(div);
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
 });
