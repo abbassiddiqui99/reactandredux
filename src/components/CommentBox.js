83fbdaaade08ed07133ee129daa40399c6bcf015
Abbas Hussain <abbas.hussain@intelligenes.net>
2021-05-17 17:25:42 +0500

    added commentBox little code

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 
-const CommentBox = () => {
-  return <div>CommentBox</div>;
-};
+class CommentBox extends Component {
+  state = { comment: "" };
+  handleChange = (e) => {
+    this.setState({ comment: e.target.value });
+  };
+  handleSumbit = (e) => {
+    e.preventDefault();
+    this.setState({ comment: "" });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSumbit}>
+        <h4>Add a Comment</h4>
+        <textarea value={this.state.comment} onChange={this.handleChange} />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    );
+  }
+}
 
 export default CommentBox;
